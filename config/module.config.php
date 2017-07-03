f7e3325eedd73cac2bd193fcae9cd46b768f801a
Mario Trojan <mario.trojan@googlemail.com>
2017-07-03 11:13:02 +0200

    added PDAProxyController

--- config/module.config.php
+++ config/module.config.php
@@ -1,11 +1,11 @@
 <?php
 namespace TueLib\Module\Configuration;
 
-
 $config = array(
     'controllers' => array(
         'invokables' => array(
             'proxy' => 'TueLib\Controller\ProxyController',
+            'pdaproxy' => 'TueLib\Controller\PDAProxyController'
         ),
     ),
     'router' => array(
@@ -20,8 +20,18 @@ $config = array(
                     )
                 )
             ),
-        ),
-    ),
+            'pdaproxy-load' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/PDAProxy/Load',
+                    'defaults' => array(
+                        'controller' => 'PDAProxy',
+                        'action'     => 'Load',
+                    )
+                )
+            )
+        )
+    )
 );
 
 return $config;
